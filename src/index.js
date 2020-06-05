7dd7673020b7fcc6fb920b93a7019fc975102c92
Bobby Harris <bobby.harris@gmail.com>
2020-06-05 19:00:34 -0400

    added firebase

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import firebase from 'firebase'
 import React from 'react'
 import { render } from 'react-dom'
 import { configureStore } from '@reduxjs/toolkit'
@@ -15,6 +16,20 @@ const store = configureStore({ reducer: rootReducer })
 if (process.env.NODE_ENV === "development") {
   makeServer({ environment: "development" })
 }
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDey_VtvKGXjbaef-a2vLKryPYAmBHMPJ0",
+  authDomain: "tstpcvetting.firebaseapp.com",
+  databaseURL: "https://tstpcvetting.firebaseio.com",
+  projectId: "tstpcvetting",
+  storageBucket: "tstpcvetting.appspot.com",
+  messagingSenderId: "1039565603932",
+  appId: "1:1039565603932:web:cdc1c14da88e0a9e229299",
+  measurementId: "G-BVN231R9XG"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 render(
   <Provider store={store}>
